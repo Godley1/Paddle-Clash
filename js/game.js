5c28287d3bdee9ff8f8eff04684065b63f8a89e5
Godley1 <bsagodley@gmail.com>
2026-04-22 09:29:10 -0700

    Add initial game loading screen to game.js

---
+++ js/game.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "cyan";
+ctx.font = "40px Arial";
+ctx.fillText("Neon Clash Loading...", 250, 250);
